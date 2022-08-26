764326fd5728faa6af61a373bd410fae1531528d
Armine <armine.silva@aluno.faculdadeimpacta.com.br>
2022-08-26 20:03:05 -0300

    remocao endpoint ngrok

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -8,7 +8,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-      fetch('https://eb99-2804-431-c7f4-db6b-4bd-a503-757b-4f47.sa.ngrok.io/api/products') //back http://localhost:7000
+      fetch('http://localhost:7000/api/products') //back http://localhost:7000
         .then(response => response.json())
         .then(data => setProducts(data))
     }, [])
