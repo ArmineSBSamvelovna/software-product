0645b986ea40064c0beb57d5fd494c3a0c4db040
Armine <armine.silva@aluno.faculdadeimpacta.com.br>
2022-09-21 00:00:44 -0300

    centralizacao e aplicacao interceptores axios

--- client/src/features/sobre/AboutPage.tsx
+++ client/src/features/sobre/AboutPage.tsx
@@ -1,10 +1,19 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage() {
     return (
-        <Typography variant='h2'>
-            Sobre
-        </Typography>
+        <Container>
+            <Typography gutterBottom variant='h2'>Teste</Typography>
+            <ButtonGroup>
+                <Button variant='contained' onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>400</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>401</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>404</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>500</Button>
+                <Button variant='contained' onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>validação</Button>
+            </ButtonGroup>
+        </Container>
+        
     )
     
 }
